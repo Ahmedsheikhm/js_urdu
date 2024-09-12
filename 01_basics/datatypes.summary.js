9aafc8a5c7a26c084b3c54cd53159c5a156e0783
Ahmedsheikhm <158795748+Ahmedsheikhm@users.noreply.github.com>
2024-09-12 14:36:31 +0000

    add_stack_and_heap

--- 01_basics/datatypes.summary.js
+++ 01_basics/datatypes.summary.js
@@ -29,3 +29,37 @@ const myFunction = function(){
     console.log("hello World");
 }
 myFunction();
+
+///////////////////////////////////////////////
+stack and heap memory
+//memory control reduced in modern languages
+//imp for interview
+//primative
+//non primative (refrence type)
+
+//two types of memories(Stack and heap)
+//all primative datatypes use stack(copy of original value)
+//all refrence datatype uses heap(refrence given to original value)
+ 
+
+//primative datatypes using stack
+
+const value1 =100
+const value2 =value1;
+const value2 =200;
+// the value for value1 will remain 100 and the value for value2 will be 200
+
+
+//non primative(refrencetype datatypes ) will use the heap
+let value1 = {
+   name:"ahmed",
+   age:19,
+}
+let value2=value1;
+//value1 and value2 both now point to the same refrence
+value2.name = "mahad";
+//doing this causes value1 and value2 to both have {
+  name:"mahad",
+  age:19,
+}
+since both refrence the same object
